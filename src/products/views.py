1c5a2006a94b0073e86fbbfd75b0158b50de28d1
k2lsuthar@gmail.com <k2lsuthar@gmail.com>
2020-07-21 01:44:20 -0400

    Used modelmanager

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,7 @@
 #from django.views import ListView
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 # Create your views here.
 from .models import  Product
@@ -21,3 +22,38 @@ def product_list_view(request):
         'object_list' : queryset
     }
     return render(request,"products/list.html",context)
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args,**kwargs)
+    #     print(context)
+    #     return context
+
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    #instance = Product.object.get(pk=pk) #id
+    #instance = get_object_or_404(Product, pk=pk)
+
+    # try:
+    #     instance = Product.object.get(pk=pk)
+    # except Product.DoesNotExist:
+    #     print("No product here")
+    #     raise Http404("Product does not exist")
+    # except:
+    #     print("huh?")
+
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
+        raise Http404("Product doesn't exist")
+    # print(instance)
+    # qs = Product.object.filter(id=pk)
+    # if qs.exists() and qs.count() == 1:
+    #     instance = qs.first()
+    context = {
+        'object' : instance
+    }
+    return render(request,"products/detail.html", context)
