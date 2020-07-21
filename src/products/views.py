253a9328adf3ea31a35010f4e9b0713b56f3baf9
k2lsuthar@gmail.com <k2lsuthar@gmail.com>
2020-07-21 00:15:07 -0400

    Commented get_context_data

--- src/products/views.py
+++ src/products/views.py
@@ -9,10 +9,10 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(ProductListView, self).get_context_data(*args,**kwargs)
-        print(context)
-        return context
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args,**kwargs)
+    #     print(context)
+    #     return context
 
 
 def product_list_view(request):
