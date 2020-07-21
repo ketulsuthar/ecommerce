873fbdc91c45b36ebd4e1f5e1a406104d00cee73
k2lsuthar@gmail.com <k2lsuthar@gmail.com>
2020-07-20 23:50:22 -0400

    created product_list_view function based

--- src/products/views.py
+++ src/products/views.py
@@ -6,3 +6,10 @@ from .models import  Product
 
 class ProductListView(ListView):
     queryset = Product.objects.all()
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list' : queryset
+    }
+    return render(request,"product/product_list_view.html",context)
