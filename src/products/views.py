4c4c68713409c8460a6e2c4132c3f51aed722a75
k2lsuthar@gmail.com <k2lsuthar@gmail.com>
2020-07-21 00:01:01 -0400

    updated product_list_view

--- src/products/views.py
+++ src/products/views.py
@@ -14,4 +14,4 @@ def product_list_view(request):
     context = {
         'object_list' : queryset
     }
-    return render(request,"products/product_list_view.html",context)
+    return render(request,"products/list.html",context)
