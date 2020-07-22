53939494d4736c4e3fb66cdbccf205a2809e4566
k2lsuthar@gmail.com <k2lsuthar@gmail.com>
2020-07-21 22:49:29 -0400

    updated views for feature field

--- src/products/views.py
+++ src/products/views.py
@@ -6,14 +6,33 @@ from django.http import Http404
 # Create your views here.
 from .models import  Product
 
+
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self,  *args, **kwargs):
+        request = self.request
+        return  Product.objects.featured()
+
+class ProductFeaturedDetailView(DetailView):
+    template_name = "products/featured-detail.html"
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
 class ProductListView(ListView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/list.html"
 
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListView, self).get_context_data(*args,**kwargs)
     #     print(context)
     #     return context
+
+    def get_queryset(self,  *args, **kwargs):
+        request = self.request
+        return  Product.objects.all()
 
 
 def product_list_view(request):
@@ -25,13 +44,26 @@ def product_list_view(request):
 
 
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args,**kwargs)
-    #     print(context)
-    #     return context
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args,**kwargs)
+        print(context)
+        return context
+
+    def get_object(self, *args, **kwargs):
+        request =self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
+
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #     return Product.objects.filter(pk=pk)
 
 
 def product_detail_view(request, pk=None, *args, **kwargs):
