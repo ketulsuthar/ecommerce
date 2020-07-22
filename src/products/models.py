40ec5e884298868863d57e46b54ef5189314926e
k2lsuthar@gmail.com <k2lsuthar@gmail.com>
2020-07-21 22:48:10 -0400

    added more fields

--- src/products/models.py
+++ src/products/models.py
@@ -18,8 +18,12 @@ def upload_image_path(instance, filename):
     return "products/{new_filename}/{final_filename}".format(new_filename=new_filename,final_filename=final_filename)
 
 class ProductManager(models.Manager):
+
+    def featured(self):
+        return self.get_queryset.filter(featured=True)
+
     def get_by_id(self, id):
-        qs = self.get_by_id().filter(id=id)
+        qs = self.get_queryset().filter(id=id)
         if qs.count() == 1:
             return qs.first()
         return None
@@ -29,6 +33,8 @@ class Product(models.Model):
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2,max_digits=20,default=39.99)
     image           = models.ImageField(upload_to='products/', null=True, blank=True)
+    featured        = models.BooleanField(default=False)
+
 
     objects = ProductManager()
 
