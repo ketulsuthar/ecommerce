6a8252f7b3c1a1ade20bc55d2794e5e7bfe9ec70
k2lsuthar@gmail.com <k2lsuthar@gmail.com>
2020-07-22 00:40:49 -0400

    added signal and slug

--- src/products/models.py
+++ src/products/models.py
@@ -1,8 +1,10 @@
 import random
 import os
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save
 # Create your models here.
+
+from .utils import  unique_slug_generator
 
 def get_file_extension(filepath):
     base_name = os.path.basename(filepath)
@@ -42,6 +44,7 @@ class ProductManager(models.Manager):
 
 class Product(models.Model):
     title           = models.CharField(max_length=120)
+    slug            = models.SlugField(blank=True,unique=True)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2,max_digits=20,default=39.99)
     image           = models.ImageField(upload_to='products/', null=True, blank=True)
@@ -56,3 +59,11 @@ class Product(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+def product_pre_save_receiver(sender,instance,*args,**kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
